b17f14b687afa9bdfe890b7f536f698fe83d9b4c
yusupscopes <yusupmaulana950@gmail.com>
2025-05-01 22:18:51 +0700

    fix: logger fixing

--- src/main.py
+++ src/main.py
@@ -5,7 +5,11 @@ from mangum import Mangum
 from uuid import uuid4
 from src.metrics import publish_metric
 import logging
-import watchtower
+import json
+
+# Configure logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 s3 = boto3.client('s3')
 BUCKET_NAME = os.environ.get("BUCKET_NAME", "")
@@ -16,37 +20,27 @@ app = FastAPI()
 # Lambda entry point
 handler = Mangum(app)
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-logger.addHandler(watchtower.CloudWatchLogHandler(log_group='ImageUploadFunction'))
-
-# Log requests and responses
-@app.middleware("http")
-async def log_requests(request, call_next):
-    logger.info(f"Request: {request.method} {request.url}")
-    response = await call_next(request)
-    logger.info(f"Response: {response.status_code}")
-    return response
-
 # Upload image to S3
 @app.post("/upload")
 async def upload_image(file: UploadFile = File(...)):
-    file_extension = file.filename.split('.')[-1]
-    file_key = f"{UPLOAD_PREFIX}{uuid4()}.{file_extension}"
+    try:
+        logger.info(f"Processing file: {json.dumps(file)}")
+        file_extension = file.filename.split('.')[-1]
+        file_key = f"{UPLOAD_PREFIX}{uuid4()}.{file_extension}"
 
-    file_content = await file.read()
+        file_content = await file.read()
 
-    s3.put_object(
-        Bucket=BUCKET_NAME,
-        Key=file_key,
-        Body=file_content,
-        ContentType=file.content_type
-    )
+        s3.put_object(
+            Bucket=BUCKET_NAME,
+            Key=file_key,
+            Body=file_content,
+            ContentType=file.content_type
+        )
 
-    publish_metric("ImageUploaded", 1)
+        publish_metric("ImageUploaded", 1)
 
-    return {
-        "message": "Image uploaded successfully",
-        "original_key": file_key
-    }
+        return {"statusCode": 200, "body": json.dumps({"message": "Image uploaded successfully!"})}
+        
+    except Exception as e:
+        logger.error(f"Error processing file: {str(e)}", exc_info=True)
+        return {"statusCode": 500, "body": json.dumps({"message": "Error processing file!"})}
